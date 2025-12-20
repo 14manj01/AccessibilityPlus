a0e89f9856ed45e4f1f68fec79b1cbc66fc3bbfc
14manj01 <14manj01@gmail.com>
2025-12-20 14:51:46 -0500

    Initial Build

--- src/test/java/com/example/ExamplePluginTest.java
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import com.sandcrabcombatnotifier.SandCrabCombatNotifierPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import org.junit.Test;
 
 public class ExamplePluginTest
 {
-	public static void main(String[] args) throws Exception
+	@Test
+	public void runClient() throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
-		RuneLite.main(args);
+		ExternalPluginManager.loadBuiltin(SandCrabCombatNotifierPlugin.class);
+		RuneLite.main(new String[]{});
+
+		// Keep the JVM alive so the client stays open when run under Gradle test
+		Thread.currentThread().join();
 	}
 }
